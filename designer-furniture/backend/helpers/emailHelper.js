ce1c60205116c23aeec77536aa9a004928b7cb44
sourave das <souravedas444@gmail.com>
2025-04-02 23:20:16 +0600

    vite update

--- designer-furniture/backend/helpers/emailHelper.js
+++ designer-furniture/backend/helpers/emailHelper.js
@@ -1,27 +1,27 @@
-import nodemailer from "nodemailer";
+import nodemailer from "nodemailer"
 
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-      user: process.env.EMAIL_USER, // Your Gmail email
-      pass: process.env.EMAIL_PASS, // Gmail App password
+      user: process.env.EMAIL_USER, 
+      pass: process.env.EMAIL_PASS,
     },
-  });
+  })
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to,
     subject,
     text,
-  };
+  }
 
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully:", info.response);
+    const info = await transporter.sendMail(mailOptions)
+    console.log("Email sent successfully:", info.response)
   } catch (error) {
-    console.error("Email Sending Error:", error);
+    console.error("Email Sending Error:", error)
   }
-};
+}
 
-export default sendEmail;
+export default sendEmail
